cf6cf023607507fe0ee4f28819bc1eb55b2ea107
Kevin Lupera <kevin.lupera@alegra.com>
2023-07-31 20:36:50 -0500

    Primer commit

---
+++ pages/currency/[id].vue
@@ -0,0 +1,43 @@
+<script setup>
+const route = useRoute();
+const { data } = await useFetch('/api/ticker?id=' + route.params.id);
+const coin = data.value[0];
+</script>
+
+<template>
+    <div class='flex items-center justify-center min-h-screen from-gray-800 via-greeen-300 to-blue-500 bg-gradient-to-br'>
+        <div class='w-full max-w-lg px-0 py-8 mx-auto bg-white rounded-lg shadow-xl'>
+            <div class='max-w-md mx-auto space-y-6'>
+                <div class="py-8 px-0 mx-auto max-w-screen-xl lg:py-16">
+                    <div class="mx-auto max-w-screen-sm text-center">
+                        <div>
+                            <h2 class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl">{{
+                                coin.name }} Detail page</h2>
+                            <table border="1 px solid" class="table-auto">
+                                <thead class="bg-gray-200 border-b">
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Symbol
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Rank</th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Price - US
+                                        $</th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Market Cap
+                                        - US $</th>
+                                </thead>
+                                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ coin.symbol
+                                    }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ coin.rank
+                                    }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{
+                                        coin.price_usd }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{
+                                        coin.market_cap_usd }}</td>
+                                </tr>
+                            </table>
+                        </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div></template>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<script setup>
+const { data } = await useFetch('/api/tickers/?limit=10')
+console.log("🚀 ~ file: index.vue:3 ~ data:", data)
+</script>
+
+<template>
+  <main>
+    <div class='flex items-center justify-center min-h-screen from-gray-800 via-greeen-300 to-blue-500 bg-gradient-to-br'>
+      <div class='w-full max-w-lg px-0 py-8 mx-auto bg-white rounded-lg shadow-xl'>
+        <div class='max-w-md mx-auto space-y-6'>
+          <div class="py-8 px-0 mx-auto max-w-screen-xl lg:py-16">
+            <div class="mx-auto max-w-screen-sm text-center">
+              <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl">Index Page</h1>
+              <table>
+                <thead class="bg-gray-200 border-b">
+                  <tr>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Name</th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Symbol</th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Price</th>
+                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Details</th>
+                  </tr>
+                </thead>
+                <tr v-for="currency in data.data" :key="data.data.id"
+                  class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{ currency.name }}</td>
+                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ currency.symbol }}</td>
+                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">{{ currency.price_usd }}</td>
+                  <td class="text-sm text-blue-800 font-light px-6 py-4 whitespace-nowrap">
+                    <NuxtLink :to="'/currency/' + currency.id">{{ currency.id }}</NuxtLink>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
